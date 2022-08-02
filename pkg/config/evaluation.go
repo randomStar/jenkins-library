86c1b65412ab27043fe29f93682d0f5263a4997d
“Raman <raman_susla@epam.com>
2022-08-02 13:35:28 +0300

    add more logging

--- pkg/config/evaluation.go
+++ pkg/config/evaluation.go
@@ -194,7 +194,6 @@ func (s *StepCondition) evaluateV1(config StepConfig, utils piperutils.FileUtils
 	}
 }
 
-<<<<<<< HEAD
 func checkConfigKeyV1(config map[string]interface{}, configKey []string) (bool, error) {
 	value, ok := config[configKey[0]]
 	if len(configKey) == 1 {
@@ -205,31 +204,7 @@ func checkConfigKeyV1(config map[string]interface{}, configKey []string) (bool, 
 		return false, nil
 	}
 	return checkConfigKeyV1(castedValue, configKey[1:])
-=======
-func checkConfigKeyV1(key string, config StepConfig) (bool, error) {
-	if strings.Contains(key, "/") {
-		listKeys := strings.Split(key, "/")
-		var raw interface{} = config.Config
-		var ok bool
-		lastIndex := len(listKeys) - 1
-		for i, key := range listKeys {
-			currentConfig, casted := raw.(map[string]interface{})
-			if !casted {
-				break
-			}
-			if raw, ok = currentConfig[key]; ok && lastIndex == i {
-				return true, nil
-			}
-		}
-	} else {
-		if configValue := config.Config[key]; configValue != nil {
-			return true, nil
-		}
-	}
-	return false, nil
->>>>>>> e6a5cf19 (fix checkIfStepActive for ConfigKey)
 }
-
 // EvaluateConditions validates stage conditions and updates runSteps in runConfig
 func (r *RunConfig) evaluateConditions(config *Config, filters map[string]StepFilters, parameters map[string][]StepParameters,
 	secrets map[string][]StepSecrets, stepAliases map[string][]Alias, glob func(pattern string) (matches []string, err error)) error {
