a6289387d9ff2cd9ebef1d53560329e903ff5cdd
Vyacheslav Starostin <vyacheslav.starostin@sap.com>
2022-08-10 13:04:41 +0600

    update

--- cmd/golangBuild.go
+++ cmd/golangBuild.go
@@ -519,6 +519,9 @@ func runGolangBuildPerArchitecture(config *golangBuildOptions, goModFile *modfil
 	} else {
 		// use default name in case no name is defined via Output
 		binaryName := path.Base(goModFile.Module.Mod.Path)
+
+		fmt.Printf("\n%s\n\n", binaryName)
+
 		binaryNames = append(binaryNames, binaryName)
 	}
 	buildOptions = append(buildOptions, config.BuildFlags...)
