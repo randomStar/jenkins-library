0cc174e8bcc8eaa1cda4bcf96dad33fc86ca52a0
Vyacheslav Starostin <vyacheslav.starostin@sap.com>
2022-09-01 02:01:28 +0600

    update helm step

--- pkg/kubernetes/helm.go
+++ pkg/kubernetes/helm.go
@@ -380,7 +380,7 @@ func (h *HelmExecute) RunHelmDependency() error {
 	}
 
 	// ******
-	err := h.utils.Chmod(filepath.Join(h.config.ChartPath, "charts"), 0007)
+	err := h.utils.Chmod(filepath.Join(h.config.ChartPath, "charts"), 0070)
 	if err != nil {
 		fmt.Println("failed to change permissions: %w", err)
 	}
